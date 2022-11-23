56ff70c2e8e0d68eed46515be7102b227b9545ea
MubarikJ <115777997+MubarikJ@users.noreply.github.com>
2022-11-23 11:22:32 +0100

    movement and jumping
    MJ

---
+++ GameJam2022/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=24-BkpFSZuI&ab_channel=bendux 
+    // Youtuber Bendux (23-11-2022)
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float JumpingPower = 11f;
+    private bool isFacingRight = true;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if(!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
+        }
+
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f);
+        }
+    }
+
+
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localeScale = transform.localScale;
+        localeScale.x *= -1f;
+        transform.localScale = localeScale;
+
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
