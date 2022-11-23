bca9f95bce06d3df1e5c48ec5a3b150781650866
carlamkm <carlamoosskole@gmail.com>
2022-11-23 10:56:22 +0100

    play button starts
    if you click play you will start the game on level 1

---
+++ GameJam2022/Assets/scrips/SceneManagerButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerButtons : MonoBehaviour
+{
+    public void LoadScene(string scenName)
+    {
+        SceneManager.LoadScene(scenName);
+    }
+
+}
