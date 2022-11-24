5e91dd4e3a926437772fd5ce5f7a4642d89c5aaa
PeterRefsgaard <115778067+PeterRefsgaard@users.noreply.github.com>
2022-11-24 11:29:58 +0100

    Power ups working upload

--- GameJam2022/Assets/Scenes/Scripts/PlayerMovement.cs
+++ GameJam2022/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -13,10 +13,13 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundLayer;
 
     private float horizontal;
+    private float PowerJumpMult = 2.0f;
     private float speed = 8f;
+    private float speedBoost = 2.0f;
     private float JumpingPower = 11f;
     private bool isFacingRight = true;
-
+    private bool speedUp = true;
+    private bool PowerJump = true;
 
 
 
@@ -26,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-        if(!isFacingRight && horizontal > 0f)
+        if (!isFacingRight && horizontal > 0f)
         {
             Flip();
         }
@@ -42,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
         }
 
-        if(context.canceled && rb.velocity.y > 0f)
+        if (context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.10f);
         }
@@ -66,5 +69,37 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void SpeedUpEnabled()
+    {
+        speedUp = true;
+        speed *= speedBoost;
+        StartCoroutine(SpeedUpDisableRoutine());
+    }
+
+    IEnumerator SpeedUpDisableRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        speed /= speedBoost;
+    }
+
+    
+    /// <summary>
+    /// Make into Jump power up.
+    /// </summary>
+    public void PowerJumpEnabled()
+    {
+        PowerJump = true;
+        JumpingPower *= PowerJumpMult;
+        StartCoroutine(PowerJumpDisableRoutine());
+    }
+
+    IEnumerator PowerJumpDisableRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        JumpingPower /= PowerJumpMult;
     }
 }
---
+++ GameJam2022/Assets/Scenes/Scripts/PowerUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerMovement player = other.transform.GetComponent<PlayerMovement>();
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.SpeedUpEnabled();
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
